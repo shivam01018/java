6faa7fc5fe9395479f069fc75c0e21c6df1e1d27
shivam01018 <61553429+shivam01018@users.noreply.github.com>
2020-03-02 22:08:48 +0530

    Add files via upload

---
+++ StringPermutation.js
@@ -0,0 +1,27 @@
+import java.util.*;
+public class StringPermutation{
+
+    static void printPermutn(String str, String ans)
+    {
+        if (str.length() == 0) {
+            System.out.print(ans + " ");
+            return;
+        }
+
+        for (int i = 0; i < str.length(); i++) {
+
+            char ch = str.charAt(i);
+            String ros = str.substring(0, i) +
+                         str.substring(i + 1);
+            printPermutn(ros, ans + ch);
+        }
+    }
+
+    public static void main(String[] args){
+
+      Scanner sc= new Scanner(System.in);
+      System.out.print("Enter a string: ");
+      String s= sc.nextLine();
+      printPermutn(s, "");
+   }
+}
